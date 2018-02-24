3392632663cdb676ec863e70e96adce361e80450
Admin <admin@Admins-Mac-mini-4.local>
2018-02-24 09:06:23 +0800

    Initial Commit

---
+++ Isip_FinalsProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Isip_FinalsProject
+//
+//  Created by Admin (TIMOTHY) on 24/02/2018.
+//  Copyright © 2018 lvcc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
